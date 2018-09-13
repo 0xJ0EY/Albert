197794a4d2b30d0a613fb6bb41ec34e89ea62ea1
Joey de Ruiter <contact@joeyderuiter.me>
2018-09-14 01:00:49 +0200

    First commit

---
+++ src/main/java/albert/Client.java
@@ -0,0 +1,16 @@
+package albert;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
