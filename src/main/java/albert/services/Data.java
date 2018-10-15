32da17739d4197a5de2a49162e5d9e68597dd7e2
Alexander <alexiaan.vd@hotmail.com>
2018-10-15 13:45:24 +0200

    interface generator added

---
+++ src/main/java/albert/services/Data.java
@@ -0,0 +1,48 @@
+package albert.services;
+
+import java.util.Date;
+
+public interface Data {
+
+    public String getFirstname();
+
+
+    public void setFirstname(String firstname);
+
+
+
+    public String getLastname();
+
+
+    public void setLastname(String lastname);
+
+
+    public String getStreet();
+
+
+
+    public void setStreet(String street);
+
+
+
+    public String getZipCode();
+
+
+
+    public void setZipCode(String zipCode);
+
+
+    public String getCity();
+
+
+
+    public void setCity(String city);
+
+
+    public Date getDate();
+
+
+
+    public void setDate(Date date);
+
+}
