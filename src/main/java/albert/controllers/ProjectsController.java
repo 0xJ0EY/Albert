03a195bba67bd616e2e85c1e779d78171d39f139
maartenberden <maartenberden96@hotmail.com>
2018-10-29 12:56:27 +0100

    tables die niet werken

--- src/main/java/albert/controllers/ProjectsController.java
+++ src/main/java/albert/controllers/ProjectsController.java
@@ -1,5 +1,6 @@
 package albert.controllers;
 
+import query.Query;
 import router.pages.EditPage;
 import router.pages.OverviewPage;
 import router.templates.TemplateController;
@@ -8,6 +9,12 @@ import router.views.PageView;
 import router.Request;
 import router.response.Response;
 import router.response.ViewResponse;
+import table.Column;
+import table.Table;
+import table.factories.cells.TextCellViewFactory;
+import table.factories.header.LeftHeaderViewFactory;
+import table.strategies.DatabaseStrategy;
+import table.views.tables.SearchTableView;
 
 public class ProjectsController extends PageController implements OverviewPage, DetailPage, EditPage {
 
@@ -16,6 +23,35 @@ public class ProjectsController extends PageController implements OverviewPage, 
             TemplateController template
     ) {
         super(view, template);
+    }
+
+    public Table getOverviewTable(){
+        Table table = new Table(
+                new DatabaseStrategy(Query.table("project")),
+                new SearchTableView()
+        );
+
+        table.addCol(new Column("project_id",
+                new LeftHeaderViewFactory("Project ID"),
+                new TextCellViewFactory())
+        );
+
+        table.addCol(new Column("name",
+                new LeftHeaderViewFactory("Naam"),
+                new TextCellViewFactory())
+        );
+
+        table.addCol(new Column("created_at",
+                new LeftHeaderViewFactory("Aangemaakt op"),
+                new TextCellViewFactory())
+        );
+
+        table.addCol(new Column("afgerond",
+                new LeftHeaderViewFactory("Afgerond"),
+                new TextCellViewFactory())
+        );
+
+        return  table;
     }
 
     @Override
